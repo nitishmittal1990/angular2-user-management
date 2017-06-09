cac64e0e7cbd75b3373147f32488f07943355432
Nitish Mittal <nitishmittal1990@gmail.com>
2017-06-09 11:57:59 +0530

    first commit

---
+++ app/_models/user.ts
@@ -0,0 +1,14 @@
+﻿export class User {
+    id: number;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    gender: string;
+    dob: string;
+    city: string;
+    pincode: number;
+    country: string;
+    isActive: boolean;
+}
